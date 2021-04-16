41a20544e932d0f7ce6bd5b1d95964e83cde434d
azeam <dennishagg@gmail.com>
2021-04-16 10:53:25 +0200

    added star

--- students_spring/src/main/java/com/alexis/students/controller/StudentController.java
+++ students_spring/src/main/java/com/alexis/students/controller/StudentController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*")
 @RequestMapping("student")
 public class StudentController {
 
